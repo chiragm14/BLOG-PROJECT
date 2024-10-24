a07943b549e14bbd51b4d51106ecec7dc9fbfca2
Chirag <chiragmali141@gmail.com>
2024-10-24 19:57:06 +0530

    Mega Blog Project

---
+++ src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Logo() {
+  return (
+    <div>
+      <img src="https://images.pexels.com/photos/572056/pexels-photo-572056.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Logo" width = '100px'/>
+    </div>
+  )
+}
+
+export default Logo
